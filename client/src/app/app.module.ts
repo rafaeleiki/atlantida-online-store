1a1a1c67a1a32f5a695063c3015ee2aa267925a8
Rafael <rafaeleiki@yahoo.com.br>
2017-10-28 11:49:12 -0200

    Added FAQ

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import {NavBarComponent} from './layout/nav-bar.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'help', component: HelpComponent },
   { path: '',
     redirectTo: '/home',
     pathMatch: 'full'
@@ -24,6 +26,7 @@ const appRoutes: Routes = [
     NavBarComponent,
     HomeComponent,
     AboutComponent,
+    HelpComponent,
   ],
   imports: [
     BrowserModule,
