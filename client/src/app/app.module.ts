1685ffa2f615a568ff223f6bc36957e92a49ea58
Rafael <rafaeleiki@yahoo.com.br>
2017-10-28 11:14:57 -0200

    Refactored to create homepage

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import {NavBarComponent} from './layout/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
