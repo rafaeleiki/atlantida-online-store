20ca0fb0561274ec0c9bf6888b8b43188e5d49ce
Unknown <zwanziger@live.com>
2017-10-31 16:50:27 -0200

    Star reviews corrected

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -14,11 +14,13 @@ private product: Product;
 private nums = [];
 private currentRate = 3;
 private reviews = [
-  {user: "leila", title: "achei um lixo", body: "muito bom amei"},
-  {user: "eiki", title: "gostei", body: "que tiroooo pisa menos 10/10"}
+  {user: "leila", title: "achei um lixo", body: "muito bom amei", review: 10, array:[]},
+  {user: "eiki", title: "gostei", body: "que tiroooo pisa menos 10/10", review: 8,array:[] }
 ]
+private finalreview = [];
 private pictures = ['assets/img/guilherme.jpeg','assets/img/leila.jpeg','assets/img/eiki.jpeg',
 'assets/img/lamas.jpeg','assets/img/tales.jpeg'];
+
 
 constructor(
 private route: ActivatedRoute,
@@ -32,6 +34,23 @@ this.route.paramMap
 this.product = product;
 for(let i = 1; i<= Math.min(this.product.stock, 10); i++){
   this.nums.push(i);
+}
+var sum = 0
+for (var i = 0; i < this.reviews.length; i++) {
+  sum += this.reviews[i].review;
+  for (var j = 0; j < this.reviews[i].review; j++) {
+    this.reviews[i].array.push(1);
+  }
+  for (var j = 0; j < (10-this.reviews[i].review); j++) {
+    this.reviews[i].array.push(0);
+  }
+}
+sum = sum/i;
+for (var j = 0; j < sum; j++) {
+  this.finalreview.push(1);
+}
+for (var j = 0; j < (10-sum); j++) {
+  this.finalreview.push(0);
 }
 });
 }
