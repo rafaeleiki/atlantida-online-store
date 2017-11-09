837d500f394ecff4045991298140dcdf78d31e5c
Rafael <rafaeleiki@yahoo.com.br>
2017-11-09 15:36:09 -0200

    Added brands filter

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -3,6 +3,11 @@ import {ProductsService} from "../integrations/products/products.service";
 import {Product} from "../integrations/products/products";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 
+interface Brand {
+  name: string;
+  include: boolean;
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -12,6 +17,8 @@ export class SearchComponent implements OnInit {
 
   ordering: string;
   searchResults: Product[];
+  products: Product[];
+  brands: Brand[];
 
   constructor(private productService: ProductsService,
               private route: ActivatedRoute) { }
@@ -24,28 +31,41 @@ export class SearchComponent implements OnInit {
       })
       .subscribe((products: Product[]) => {
         this.searchResults = products;
+        this.products = products;
+
+        const brandNames = Array.from(new Set(products.map(p => p.brand)));
+        this.brands = brandNames.map(name => ({ name, include: true }));
       });
   }
 
   orderBy() {
     if (this[this.ordering]) {
-      this.searchResults = this[this.ordering](this.searchResults);
+      this[this.ordering]();
     }
   }
 
-  sortAsc(products: Product[]): Product[] {
-    return products.sort((a, b) => a.name.localeCompare(b.name));
+  filter() {
+    this.products = this.searchResults;
+    this.brands.forEach(brand => {
+      if (!brand.include) {
+        this.products = this.products.filter(product => product.brand !== brand.name);
+      }
+    });
   }
 
-  sortDsc(products: Product[]): Product[] {
-    return products.sort((a, b) => b.name.localeCompare(a.name));
+  sortAsc() {
+    this.products = this.products.sort((a, b) => a.name.localeCompare(b.name));
   }
 
-  sortAscVal(products: Product[]): Product[] {
-    return products.sort((a, b) => a.price - b.price);
+  sortDsc() {
+    this.products = this.products.sort((a, b) => b.name.localeCompare(a.name));
   }
 
-  sortDscVal(products: Product[]): Product[] {
-    return products.sort((a, b) => b.price - a.price);
+  sortAscVal() {
+    this.products = this.products.sort((a, b) => a.price - b.price);
+  }
+
+  sortDscVal() {
+    this.products = this.products.sort((a, b) => b.price - a.price);
   }
 }
