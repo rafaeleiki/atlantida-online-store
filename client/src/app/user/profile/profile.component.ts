f2502c5ffae250d4bb6052f3e832db9871020601
Tales Lelo da Aparecida <tales.aparecida@gmail.com>
2017-11-17 17:02:05 +0000

    Modifies Edit Profile page to comply to address
    Address is filled after CEP blurs

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
 import {User} from '../user';
+import {CepService} from '../../integrations/cep/cep.service';
+import {Address} from '../../integrations/cep/cep';
+
 
 @Component({
   selector: 'app-profile',
@@ -11,11 +14,22 @@ export class ProfileComponent implements OnInit {
 
   private user: User;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private cepService: CepService) { }
 
   ngOnInit() {
     this.userService.getCurrentUser()
       .then(user => this.user = user);
+  }
+
+  getAddress() : void {
+    this.cepService.getAddress(this.user.cep)
+    .then((address : Address) => {
+      this.user.address = address;
+    })
+    .catch((error) => {
+      console.error('Error on get cep', error);
+      return Promise.reject(error.message || error);
+    });
   }
 
 }
