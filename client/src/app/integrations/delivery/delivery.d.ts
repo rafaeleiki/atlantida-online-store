6e0f5846e1e3928e4db3729fda046bc52d969ad1
Unknown <guilherme.a.valarini@gmail.com>
2017-11-17 17:14:40 -0200

    Delivery data structures
    Created structures for data acquisition

---
+++ client/src/app/integrations/delivery/delivery.d.ts
@@ -0,0 +1,54 @@
+export interface Check {
+  message: string;
+  preco: number;
+  tempo: number;
+}
+
+export interface Response {
+  message: string;
+  status: string;
+}
+
+export interface Activity {
+  id: number;
+  id_pacote: number;
+  descricao: string;
+}
+
+export interface ShippingInc {
+  id: number;
+  nome: string;
+}
+
+export interface PlaceInfo {
+  id: number;
+  cep: number;
+  numero: number;
+  estado: string;
+  cidade: string;
+  endereco: string;
+}
+
+export interface Store {
+  id: number;
+  nome: string;
+  email: string;
+  id_endereco: number;
+}
+
+export interface Delivery {
+  id: number;
+  id_transportadora: number;
+  id_site: number;
+  id_partida: number;
+  id_destino: number;
+  status: number;
+  destinatario: string;
+  atividades: [Activity];
+  transportadora: ShippingInc;
+  partida: PlaceInfo;
+  destino: PlaceInfo;
+  site: {
+    id: Store;
+  }
+}
