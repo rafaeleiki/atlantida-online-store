a97ea768624082ae7f9756448c6278cad945b4a9
Unknown <guilherme.a.valarini@gmail.com>
2017-11-23 20:12:25 -0200

    Service tested

--- client/src/app/integrations/mewketing/mewketing.service.spec.ts
+++ client/src/app/integrations/mewketing/mewketing.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MewketingService } from './mewketing.service';
+import {HttpModule} from '@angular/http';
 
 describe('MewketingService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MewketingService]
+      providers: [MewketingService],
+      imports: [HttpModule]
     });
   });
 
@@ -17,7 +19,46 @@ describe('MewketingService', () => {
   it('should send email',
     inject([MewketingService],
       (service: MewketingService) => {
-    service.sendEmail(Date.now(), 'Oi leilaaa','Email de teste', ['leila'], {nome:'leila'} )
-      .then((mewketingResponse) => expect(mewketingResponse.status).not.toBe(''));
+    service.sendEmail('2017-11-17T21:25:35.516Z', 'Oi leilaaaadasuyudsaudfasyudfaudyufasd','Email de teste', ['guilherme'],
+      {
+        vars: ["email", "nome"],
+        values: [{email:"zwanziger@live.com", nome:"leils"}]
+      })
+      .then((res) => {
+      console.log(res);
+      expect(res.status == "Email sent" || res.status == "Email schedule").toBeTruthy();
+      });
   }));
+
+  // it('should create a receiver',
+  //   inject([MewketingService],
+  //     (service: MewketingService) => service.createReceiver('teste', 'teste@teste.com')
+  //       .then(res => expect(res.status).toEqual('Receiver created'))
+  //   ));
+
+  //
+  // it('should add receiver to group',
+  //   inject([MewketingService],
+  //     (service: MewketingService) => service.joinGroup('Rolezeiros', 'teste@teste.com')
+  //       .then(res => expect(res.status).toEqual('Receiver added to group'))
+  //   ));
+
+  //
+  // it('should remove receiver from group',
+  //   inject([MewketingService],
+  //     (service: MewketingService) => service.exitGroup('Rolezeiros', 'teste@teste.com')
+  //       .then(res => expect(res.status).toEqual('Receiver removed from group'))
+  //   ));
+  //
+  // it('should update a receiver',
+  //   inject([MewketingService],
+  //     (service: MewketingService) => service.updateReceiver('teste', 'teste2', 'teste2@teste.com')
+  //       .then(res => expect(res.status).toEqual('Receiver updated'))
+  //   ));
+  //
+  // it('should remove a receiver',
+  //   inject([MewketingService],
+  //     (service: MewketingService) => service.removeReceiver('teste2')
+  //       .then(res => expect(res.status).toEqual('Receiver removed'))
+  //   ));
 });
