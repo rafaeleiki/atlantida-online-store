d8584b5ce7cababb068397d594f9ce230306263b
Rafael <rafaeleiki@yahoo.com.br>
2017-11-24 15:11:26 -0200

    Created paymens database

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -2,10 +2,14 @@
 
 var server = require('../server');
 var ds = server.dataSources.db;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = [
+  'User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Payment'
+];
 
-ds.automigrate(lbTables, function(er) {
+ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
   ds.disconnect();
 });
+
+// ds.autoupdate('Payment', function(err, result) {});
